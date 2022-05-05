5c3c5eac740af398d3d6ffaae52177941b0677b1
yym68686 <yym68686@outlook.com>
2022-05-05 23:10:25 +0800

    modify main.py

--- main.py
+++ main.py
@@ -35,7 +35,6 @@ def caps(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
 
 def inuaa(update: Update, context: CallbackContext):
-    # context.bot.send_message(chat_id=update.effective_chat.id, text=context.args[1])
     result = startinuaa(context.args[0], context.args[1])
     context.bot.send_message(chat_id=update.effective_chat.id, text=result)
 
