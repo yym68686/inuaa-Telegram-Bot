d84a2924252a5d84b60386edc5ab036cd442449e
yym68686 <yym68686@outlook.com>
2022-06-08 18:16:14 +0800

    add Automatic clock in

--- main.py
+++ main.py
@@ -1,26 +1,55 @@
 import os
 import sys
+import time
 import logging
+import schedule
+import NotionDatabase
 from nuaa import startinuaa
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from threading import Thread
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
 
 TOKEN = os.getenv("TOKEN") # 从环境变量自动获取telegram bot Token
 MODE = os.getenv("MODE")
+
 PORT = int(os.environ.get('PORT', '8443'))
 HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
+DATABASEID = os.getenv("DATABASEID")
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger()
 
 def start(update, context): # 当用户输入/start时，返回文本
-    update.message.reply_text('欢迎使用 🎉')
+    user = update.effective_user
+    update.message.reply_html(
+        rf"Hi {user.mention_html()} 欢迎使用 🎉",
+        reply_markup=ForceReply(selective=True),
+    )
 
 def help(update, context):
     update.message.reply_text('我是人见人爱的yym的小跟班~')
 
 def echo(update, context):
     update.message.reply_text(update.message.text)
+
+def Inline(update: Update, context: CallbackContext):
+    keyboard = [
+        [
+            InlineKeyboardButton("Option 1", callback_data='1'),
+            InlineKeyboardButton("Option 2", callback_data='2'),
+        ],
+        [
+            InlineKeyboardButton("Option 3", callback_data='3'),
+        ]
+    ] #1
+    reply_markup = InlineKeyboardMarkup(keyboard) #2
+    update.message.reply_text("Please choose:", reply_markup=reply_markup) #3
+
+
+def keyboard_callback(update: Update, context: CallbackContext): #4
+    query = update.callback_query #5
+    query.answer() #6
+    query.edit_message_text(text=f"Selected option: {query.data}") #7
 
 def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
@@ -31,6 +60,36 @@ def unknown(update: Update, context: CallbackContext): # 当用户输入未知�
 def caps(update: Update, context: CallbackContext): # 小的测试功能，也是官方示例，将用户参数转化为大写
     text_caps = ' '.join(context.args).upper()
     context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+def check(update: Update, context: CallbackContext): # 调加自动打卡
+    if (len(context.args) == 2): # /check 后面必须是两个参数
+        context.bot.send_message(chat_id=update.effective_chat.id, text="欢迎使用自动打卡功能~")
+        body = {
+            'properties':{}
+        }
+        body = NotionDatabase.body_properties_input(body, 'StuID', 'title', context.args[0])
+        body = NotionDatabase.body_properties_input(body, 'password', 'rich_text', context.args[1])
+        body = NotionDatabase.body_properties_input(body, 'cookie', 'rich_text', '**')
+        body = NotionDatabase.body_properties_input(body, 'checkdaily', 'rich_text', '1')
+        body = NotionDatabase.body_properties_input(body, 'chat_id', 'rich_text', str(update.effective_chat.id))
+        result = NotionDatabase.DataBase_additem(DATABASEID, body, context.args[0])
+        context.bot.send_message(chat_id=update.effective_chat.id, text=result) # 打卡结果打印
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="格式错误哦~，请输入 /check <学号> <教务处密码>，例如学号为123，密码是123，则输入/inuaa 123 123")
+
+def dailysign():
+    Stuinfo = NotionDatabase.datafresh(NotionDatabase.DataBase_item_query(DATABASEID))
+    seen = set()
+    Stuinfo = [x for x in Stuinfo if frozenset(x) not in seen and not seen.add(frozenset(x))]
+    for item in Stuinfo:
+        updater.bot.send_message(chat_id = int(item["chat_id"]), text="自动打卡开始啦，请稍等哦，大约20秒就好啦~")
+        result = startinuaa(item['StuID'], item['password']) # 调用打卡程序
+        updater.bot.send_message(chat_id = int(item["chat_id"]), text=result) # 打卡结果打印
+
+def schedule_checker():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 def inuaa(update: Update, context: CallbackContext): # 当用户输入/inuaa 学号，密码 时，自动打卡，调用nuaa.py文件
     if (len(context.args) == 2): # /inuaa后面必须是两个参数
@@ -57,6 +116,9 @@ if __name__ == '__main__':
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help))
     dispatcher.add_handler(CommandHandler("caps", caps))
+    dispatcher.add_handler(CommandHandler("Inline", Inline))
+    dispatcher.add_handler(CommandHandler("check", check))
+    dispatcher.add_handler(CallbackQueryHandler(keyboard_callback))
     dispatcher.add_handler(CommandHandler("inuaa", inuaa)) # 当用户输入/inuaa时，调用inuaa()函数
 
     dispatcher.add_handler(MessageHandler(Filters.command, unknown))
@@ -67,5 +129,8 @@ if __name__ == '__main__':
     elif MODE == "prod": # HeroKu 远程生产环境
         updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
         updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, TOKEN))
+
+    schedule.every().day.at("18:05").do(dailysign)
+    Thread(target=schedule_checker).start() 
 
     updater.idle()
