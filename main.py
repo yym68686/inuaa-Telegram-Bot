550e95a2a41a7a103d37d87657d1b487ac9692e4
yym68686 <yym68686@outlook.com>
2022-06-10 20:44:41 +0800

    add toUTC function

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ MODE = os.getenv("MODE")
 PORT = int(os.environ.get('PORT', '8443'))
 HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
 DATABASEID = os.getenv("DATABASEID")
-checktime = '16:01'
+checktime = '00:01'
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger()
@@ -169,7 +169,7 @@ if __name__ == '__main__':
         updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
         updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, TOKEN))
 
-    schedule.every().day.at(checktime).do(dailysign)
+    schedule.every().day.at(toUTC(checktime)).do(dailysign)
     Thread(target=schedule_checker).start() 
 
     updater.idle()
