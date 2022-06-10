8b0b1d60dbdd5efee49ad465d36c19a266115206
yym68686 <yym68686@outlook.com>
2022-06-11 02:51:55 +0800

    add toUTC function

--- main.py
+++ main.py
@@ -88,6 +88,11 @@ def caps(update: Update, context: CallbackContext): # 小的测试功能，也�
     context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
 
 def adddata(person, context, StuID, password, cookie, checkdaily, chatid):
+    Stuinfo = NotionDatabase.datafresh(NotionDatabase.DataBase_item_query(DATABASEID))
+    for item in Stuinfo:
+        if (StuID == item["StuID"] and checkdaily == item["checkdaily"])
+            context.bot.send_message(chat_id=person, text="账号已添加到数据库，不需要重复添加，系统会自动为您每日打卡") # 打卡结果打印
+            return
     body = {
         'properties':{}
     }
