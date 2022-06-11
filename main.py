565aff5464e8d83c8fed2923ddb216d49f2dc1c0
yym68686 <yym68686@outlook.com>
2022-06-12 01:42:26 +0800

    add toUTC function

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ MODE = os.getenv("MODE")
 PORT = int(os.environ.get('PORT', '8443'))
 HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
 DATABASEID = os.getenv("DATABASEID")
-checktime = '01:04'
+checktime = '00:01'
 admin = 917527833
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
