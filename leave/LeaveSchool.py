ccc04947c9f5f4e9d9957ad4fdab599553c2d825
yym68686 <yym68686@outlook.com>
2022-09-03 01:38:31 +0800

    修复多线程bug

--- leave/LeaveSchool.py
+++ leave/LeaveSchool.py
@@ -10,7 +10,11 @@ from leave.config import GetStepIdraw, raw
 # 获取 cookies
 async def getJSESSIONID(username, password):
     # browser = await launch(headless=False, args=['--disable-infobars'])
-    browser = await launch()
+    browser = await launch({
+        'handleSIGINT':False,
+        'handleSIGTERM': False,
+        'handleSIGHUP': False
+    })
     page = await browser.newPage()
     # 绕过 WebDriver 的检测，在每次加载网页的时候执行语句，执行将 WebDriver 隐藏的命令
     await page.evaluateOnNewDocument('Object.defineProperty(navigator, "webdriver", {get: () => undefined})')
