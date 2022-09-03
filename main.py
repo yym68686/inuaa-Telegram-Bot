0b6630837bcbb28b36b09f58cceda675e3c417ed
yym68686 <yym68686@outlook.com>
2022-09-03 10:59:49 +0800

    add ASCII banner

--- main.py
+++ main.py
@@ -176,6 +176,9 @@ def leave(update: Update, context: CallbackContext): # 当用户输入/leave 学
 def weather(update, context):
     context.job_queue.run_daily(msg, datetime.time(hour=1, minute=56, tzinfo=pytz.timezone('Asia/Shanghai')), days=(0, 1, 2, 3, 4, 5, 6), context=admin)
 
+def speak(update, context):
+    context.job_queue.run_daily(msg, datetime.time(hour=1, minute=56, tzinfo=pytz.timezone('Asia/Shanghai')), days=(0, 1, 2, 3, 4, 5, 6), context=admin)
+
 def msg(context):
     context.bot.send_message(chat_id=context.job.context, text='定时任务')
 
