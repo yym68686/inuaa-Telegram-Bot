680f30c67a1bc74d9e5decdefbdccb64e36a4fbc
yym68686 <yym68686@outlook.com>
2022-11-19 09:56:50 +0800

    test

--- main.py
+++ main.py
@@ -135,6 +135,7 @@ def leave(update: Update, context: CallbackContext): # 当用户输入/leave 学
     context.bot.send_message(chat_id=update.effective_chat.id, text=result) # 打卡结果打印
     context.bot.send_message(chat_id=admin, text=context.args[0] + result) # 打卡结果打印
 
+
 # 小功能
 def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
